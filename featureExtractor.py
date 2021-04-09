Adding more models + adding normalization in preprocessing + fixing to_device

featureExtractor.py
@@ -120,7 +120,7 @@ class FeatureExtractor:
 
         for model in tqdm(self.models, desc="Models loop -> "):
             # load model to gpu if it's available for faster computations
-            model.to(device)
+            model = model.to(device)
 
             # pass the imgs into the model so we can capture the intermediate layers outputs.
             # self.corresponding_labels will be used in get_activation method below
@@ -135,8 +135,10 @@ class FeatureExtractor:
                         imgs, self.corresponding_labels = data
 
                         # move the imgs to gpu if it's available
-                        imgs["left"].to(device)
-                        imgs["right"].to(device)
+                        imgs["left"], imgs["right"] = (
+                            imgs["left"].to(device),
+                            imgs["right"].to(device),
+                        )
 
                         # go
                         self.save_path = self.original_save_path / "left"
@@ -158,7 +160,7 @@ class FeatureExtractor:
                     model(imgs)
 
             # re-move the model from gpu if it was available to cpu
-            model.to(torch.device("cpu"))
+            model = model.to(torch.device("cpu"))
 
             self.processor.set_path(self.original_save_path)
             self.processor.execute()

models.py
@@ -1,6 +1,9 @@
 import torch
 import torch.nn as nn
 import torchvision.models
+import timm
+
+from facenet_pytorch import InceptionResnetV1
 
 """
     This is the file that will contain the models dictionnary that will be used to compute the feature maps.
@@ -22,6 +25,9 @@ def get_layer_index(model: nn.Module, layer_name: str):
 models_dict = dict()
 
 # SUPERVISED
+
+# RESNETs
+
 resnet18 = torchvision.models.resnet18(pretrained=True).eval()
 models_dict["resnet18"] = {
     "model": resnet18,
@@ -33,14 +39,26 @@ models_dict["resnet18"] = {
     ],
 }
 
+# resnet34 = torchvision.models.resnet34(pretrained=True).eval()
 # models_dict["resnet34"] = {
 #     "model": torchvision.models.resnet34(pretrained=True).eval(),
-#     "layers": [-2],
+#     "layers": [
+#         get_layer_index(resnet34, "layer1"),
+#         get_layer_index(resnet34, "layer2"),
+#         get_layer_index(resnet34, "layer3"),
+#         get_layer_index(resnet34, "layer4"),
+#     ],
 # }
 
+# resnet50 = torchvision.models.resnet50(pretrained=True).eval()
 # models_dict["resnet50"] = {
 #     "model": torchvision.models.resnet50(pretrained=True).eval(),
-#     "layers": [-2, -3],
+#     "layers": [
+#         get_layer_index(resnet50, "layer1"),
+#         get_layer_index(resnet50, "layer2"),
+#         get_layer_index(resnet50, "layer3"),
+#         get_layer_index(resnet50, "layer4"),
+#     ],
 # }
 
 # models_dict["resnet101"] = {
@@ -52,6 +70,73 @@ models_dict["resnet18"] = {
 #     "model": torchvision.models.resnet152(pretrained=True).eval(),
 #     "layers": [-2],
 # }
+
+
+# Big Transfer
+# pretrained on imagenet21k, finetuned on imagenet1k
+
+# models_dict["BiT-M-R50x1"] = {
+#     "model": timm.create_model("resnetv2_50x1_bitm", pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
+# models_dict["BiT-M-R50x3"] = {
+#     "model": timm.create_model('resnetv2_50x3_bitm', pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
+# models_dict["BiT-M-R101x1"] = {
+#     "model": timm.create_model('resnetv2_101x1_bitm', pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
+# models_dict["BiT-M-R101x3"] = {
+#     "model": timm.create_model('resnetv2_101x3_bitm', pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
+# models_dict["BiT-M-R152x2"] = {
+#     "model": timm.create_model('resnetv2_152x2_bitm', pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
+# models_dict["BiT-M-R152x4"] = {
+#     "model": timm.create_model('resnetv2_152x4_bitm', pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
+# Big Transfer trained on imagenet-21k
+
+# models_dict["BiT-M-R50x1_in21k"] = {
+#     "model": timm.create_model('resnetv2_50x1_bitm_in21k', pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
+# models_dict["BiT-M-R50x3_in21k"] = {
+#     "model": timm.create_model('resnetv2_50x3_bitm_in21k', pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
+# models_dict["BiT-M-R101x1_in21k"] = {
+#     "model": timm.create_model('resnetv2_101x1_bitm_in21k', pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
+# models_dict["BiT-M-R101x3_in21k"] = {
+#     "model": timm.create_model('resnetv2_101x3_bitm_in21k', pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
+# models_dict["BiT-M-R152x2_in21k"] = {
+#     "model": timm.create_model('resnetv2_152x2_bitm_in21k', pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
+# models_dict["BiT-M-R152x4_in21k"] = {
+#     "model": timm.create_model('resnetv2_152x4_bitm_in21k', pretrained=True).eval(),
+#     "layers": [-2],
+# }
+
 
 # models_dict["alexnet"] = {
 #     "model": torchvision.models.alexnet(pretrained=True).eval(),
@@ -66,11 +151,11 @@ models_dict["resnet18"] = {
 #     "layers": [-2],
 # }
 
-# from facenet_pytorch import InceptionResnetV1
 # models_dict["facenet"] = {
 #     "model": InceptionResnetV1(pretrained="vggface2").eval(),
 #     "layers": [-2],
 # }
+
 
 # SEMI-SUPERVISED
 
@@ -81,6 +166,7 @@ models_dict["resnet18"] = {
 #     ).eval(),
 #     "layers": [-2],
 # }
+
 
 # SELF-SUPERVISED
 