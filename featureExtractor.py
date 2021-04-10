6c93bb73c475f7be652999d0a680f02041725286
Amine <shetsecure@gmail.com>
2021-04-10 02:33:00 +0200

    Introducing lazy loading for faster and optimized processing

--- featureExtractor.py
+++ featureExtractor.py
@@ -6,13 +6,15 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 
 from torch.utils.data import Dataset, DataLoader
-from dataset import TTLDataset, SimpleDataset
-from processor import Processor
 from pathlib import Path
 from typing import List, Tuple, Dict
 from types import FunctionType
 from collections.abc import Iterable
 from tqdm import tqdm
+
+from dataset import TTLDataset, SimpleDataset
+from processor import Processor
+from models import get_layer_index
 
 
 class FeatureExtractor:
@@ -28,8 +30,8 @@ class FeatureExtractor:
 
         models_dict (str)          : dictionnary that contains the models that we are going to use
                                      where the keys are the names of the models and the values are
-                                     a dictionnary that contains the nn.Module object and the list of layers
-                                     indices ( see models.py )
+                                     a dictionnary that contains the the callable function and
+                                     the list of layers indices ( see models.py ).
         """
         assert isinstance(processor, Processor)
         assert isinstance(models_dict, Dict)
@@ -37,22 +39,13 @@ class FeatureExtractor:
         for model_name, model_d in models_dict.items():
             assert isinstance(model_name, str)
             assert isinstance(model_d, Dict)
-            assert isinstance(model_d["model"], nn.Module)
+            assert isinstance(model_d["model"], FunctionType)
             assert isinstance(model_d["layers"], Iterable)
 
         self.processor = processor
         self.save_path = self.processor.save_path
-
-        self.models = [d["model"] for d in list(models_dict.values())]
-
-        for model_name, model_d in models_dict.items():
-            model, layers_indices = model_d["model"], model_d["layers"]
-
-            for layer_index in layers_indices:
-                layer_name, module = list(model.named_modules())[layer_index]
-                module.register_forward_hook(
-                    self.__get_activation(model_name, layer_name, processor)
-                )
+        self.models_dict = models_dict
+        self.models_names = list(models_dict.keys())
 
     @torch.no_grad()
     def extract_features_from_directory(
@@ -118,9 +111,22 @@ class FeatureExtractor:
         is_ttl = isinstance(loader.dataset, TTLDataset)
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        for model in tqdm(self.models, desc="Models loop -> "):
+        print(f"Extracting features with {len(self.models_names)} models !")
+
+        t = tqdm(self.models_names)
+        for model in t:
+            model_name = model
+            t.set_description(f"Models Loop: Loading {model_name} model...")
+            t.refresh()
+            
             # load model to gpu if it's available for faster computations
+            model = self.__construct_model(model)
             model = model.to(device)
+
+            t.set_description(
+                f"Models Loop: {model_name} is loaded. Extracting features..."
+            )
+            t.refresh()
 
             # pass the imgs into the model so we can capture the intermediate layers outputs.
             # self.corresponding_labels will be used in get_activation method below
@@ -159,11 +165,41 @@ class FeatureExtractor:
                     imgs.to(device)  # move the imgs to gpu if it's available
                     model(imgs)
 
-            # re-move the model from gpu if it was available to cpu
-            model = model.to(torch.device("cpu"))
+            # We are done with this model, let's free the memory
+            del model
 
             self.processor.set_path(self.original_save_path)
             self.processor.execute()
+
+    def __construct_model(self, model_name: str):
+        """
+        This method serves as constructor for the model and will register the forward hook accordingly.
+        What it does is:
+            Get the lambda expression in self.models_dict[model_name].
+            Construct the pytorch model.
+            register the forward hook.
+
+            return the model
+        """
+        assert model_name in self.models_names
+
+        m_dict = self.models_dict[model_name]
+        # constructing the model
+        model = m_dict["model"].__call__()
+
+        # constructing the layer indices
+        layers_indices = [
+            layer if isinstance(layer, int) else get_layer_index(model, layer)
+            for layer in m_dict["layers"]
+        ]
+
+        for layer_index in layers_indices:
+            layer_name, module = list(model.named_modules())[layer_index]
+            module.register_forward_hook(
+                self.__get_activation(model_name, layer_name, self.processor)
+            )
+
+        return model
 
     @torch.no_grad()
     def __get_activation(self, model_name: str, layer_name: str, processor: Processor):
--- models.py
+++ models.py
@@ -8,9 +8,17 @@ from facenet_pytorch import InceptionResnetV1
 """
     This is the file that will contain the models dictionnary that will be used to compute the feature maps.
 
+    The use of lambda expressions is merely a trick to not load all the models at once when executing
+    the script. Instead, we load just the lambda expressions (FunctionType), and then in the featureExtractor
+    we'll loop over all these lambdas expressions and we'll load model by model.
+    If we keep the previous version, we'll need to load all the models into memory first and then we can
+    start extracting features, which is horrible if we have a lot of models (huge time is needed to 
+    load all the models and we need a LOT of RAM), hence the use of this trick.
+
     Each value of this dictionnary must be a dict itself with 2 keys:
-        model: torch.nn.Module ( the model that we'll use to extract the features )
-        layers: List[int] ( the layers indices of which we want to extract the output of this model )
+        model: the lambda expression that will be called to load the model.
+        layers: List[int or str] ( the layers indices or names of which we want to extract
+                                   the output of this model )
 """
 
 
@@ -28,157 +36,172 @@ models_dict = dict()
 
 # RESNETs
 
-resnet18 = torchvision.models.resnet18(pretrained=True).eval()
+resnet18 = lambda *args: torchvision.models.resnet18(pretrained=True, *args).eval()
 models_dict["resnet18"] = {
     "model": resnet18,
-    "layers": [
-        get_layer_index(resnet18, "layer1"),
-        get_layer_index(resnet18, "layer2"),
-        get_layer_index(resnet18, "layer3"),
-        get_layer_index(resnet18, "layer4"),
-    ],
+    "layers": ["layer1", "layer2", "layer3", "layer4", -2, -3],
 }
 
-# resnet34 = torchvision.models.resnet34(pretrained=True).eval()
-# models_dict["resnet34"] = {
-#     "model": torchvision.models.resnet34(pretrained=True).eval(),
-#     "layers": [
-#         get_layer_index(resnet34, "layer1"),
-#         get_layer_index(resnet34, "layer2"),
-#         get_layer_index(resnet34, "layer3"),
-#         get_layer_index(resnet34, "layer4"),
-#     ],
-# }
+resnet34 = lambda *args: torchvision.models.resnet34(pretrained=True, *args).eval()
+models_dict["resnet34"] = {
+    "model": resnet34,
+    "layers": ["layer1", "layer2", "layer3", "layer4"],
+}
 
-# resnet50 = torchvision.models.resnet50(pretrained=True).eval()
-# models_dict["resnet50"] = {
-#     "model": torchvision.models.resnet50(pretrained=True).eval(),
-#     "layers": [
-#         get_layer_index(resnet50, "layer1"),
-#         get_layer_index(resnet50, "layer2"),
-#         get_layer_index(resnet50, "layer3"),
-#         get_layer_index(resnet50, "layer4"),
-#     ],
-# }
+resnet50 = lambda *args: torchvision.models.resnet50(pretrained=True, *args).eval()
+models_dict["resnet50"] = {
+    "model": resnet50,
+    "layers": ["layer1", "layer2", "layer3", "layer4"],
+}
 
-# models_dict["resnet101"] = {
-#     "model": torchvision.models.resnet101(pretrained=True).eval(),
-#     "layers": [-2],
-# }
+resnet101 = lambda *args: torchvision.models.resnet101(pretrained=True, *args).eval()
+models_dict["resnet101"] = {
+    "model": resnet101,
+    "layers": ["layer1", "layer2", "layer3", "layer4"],
+}
 
-# models_dict["resnet152"] = {
-#     "model": torchvision.models.resnet152(pretrained=True).eval(),
-#     "layers": [-2],
-# }
-
+resnet152 = lambda *args: torchvision.models.resnet152(pretrained=True, *args).eval()
+models_dict["resnet152"] = {
+    "model": resnet152,
+    "layers": ["layer1", "layer2", "layer3", "layer4"],
+}
 
 # Big Transfer
 # pretrained on imagenet21k, finetuned on imagenet1k
 
-# models_dict["BiT-M-R50x1"] = {
-#     "model": timm.create_model("resnetv2_50x1_bitm", pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R50x1"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_50x1_bitm", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
-# models_dict["BiT-M-R50x3"] = {
-#     "model": timm.create_model('resnetv2_50x3_bitm', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R50x3"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_50x3_bitm", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
-# models_dict["BiT-M-R101x1"] = {
-#     "model": timm.create_model('resnetv2_101x1_bitm', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R101x1"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_101x1_bitm", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
-# models_dict["BiT-M-R101x3"] = {
-#     "model": timm.create_model('resnetv2_101x3_bitm', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R101x3"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_101x3_bitm", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
-# models_dict["BiT-M-R152x2"] = {
-#     "model": timm.create_model('resnetv2_152x2_bitm', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R152x2"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_152x2_bitm", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
-# models_dict["BiT-M-R152x4"] = {
-#     "model": timm.create_model('resnetv2_152x4_bitm', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R152x4"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_152x4_bitm", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
 # Big Transfer trained on imagenet-21k
 
-# models_dict["BiT-M-R50x1_in21k"] = {
-#     "model": timm.create_model('resnetv2_50x1_bitm_in21k', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R50x1_in21k"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_50x1_bitm_in21k", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
-# models_dict["BiT-M-R50x3_in21k"] = {
-#     "model": timm.create_model('resnetv2_50x3_bitm_in21k', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R50x3_in21k"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_50x3_bitm_in21k", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
-# models_dict["BiT-M-R101x1_in21k"] = {
-#     "model": timm.create_model('resnetv2_101x1_bitm_in21k', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R101x1_in21k"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_101x1_bitm_in21k", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
-# models_dict["BiT-M-R101x3_in21k"] = {
-#     "model": timm.create_model('resnetv2_101x3_bitm_in21k', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R101x3_in21k"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_101x3_bitm_in21k", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
-# models_dict["BiT-M-R152x2_in21k"] = {
-#     "model": timm.create_model('resnetv2_152x2_bitm_in21k', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R152x2_in21k"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_152x2_bitm_in21k", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
-# models_dict["BiT-M-R152x4_in21k"] = {
-#     "model": timm.create_model('resnetv2_152x4_bitm_in21k', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["BiT-M-R152x4_in21k"] = {
+    "model": lambda *args: timm.create_model(
+        "resnetv2_152x4_bitm_in21k", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
 
-# models_dict["alexnet"] = {
-#     "model": torchvision.models.alexnet(pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["alexnet"] = {
+    "model": lambda *args: torchvision.models.alexnet(pretrained=True, *args).eval(),
+    "layers": [-2],
+}
+
 
 # other efficientnet versions : torch.hub.list('rwightman/gen-efficientnet-pytorch')
-# models_dict["efficientnet_b0"] = {
-#     "model": torch.hub.load(
-#         "rwightman/gen-efficientnet-pytorch", "efficientnet_b0", pretrained=True
-#     ).eval(),
-#     "layers": [-2],
-# }
+models_dict["efficientnet_b0"] = {
+    "model": lambda *args: torch.hub.load(
+        "rwightman/gen-efficientnet-pytorch", "efficientnet_b0", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
-# models_dict["facenet"] = {
-#     "model": InceptionResnetV1(pretrained="vggface2").eval(),
-#     "layers": [-2],
-# }
+models_dict["facenet"] = {
+    "model": lambda *args: InceptionResnetV1(pretrained="vggface2", *args).eval(),
+    "layers": [-2],
+}
 
 
 # Vision Transformer
 # all versions are available here: https://github.com/rwightman/pytorch-image-models/blob/master/timm/models/vision_transformer.py
-# models_dict["ViT"] = {
-#     "model": timm.create_model('vit_base_patch16_224', pretrained=True).eval(),
-#     "layers": [-2],
-# }
+models_dict["ViT"] = {
+    "model": lambda *args: timm.create_model(
+        "vit_base_patch16_224", pretrained=True, *args
+    ).eval(),
+    "layers": [-2],
+}
 
 
 # SEMI-SUPERVISED
 
 # facebook semi-supervised models: https://github.com/facebookresearch/semi-supervised-ImageNet1K-models
-# models_dict["resnet50_swsl"] = {
-#     "model": torch.hub.load(
-#         "facebookresearch/semi-supervised-ImageNet1K-models", "resnet50_swsl"
-#     ).eval(),
-#     "layers": [-2],
-# }
+models_dict["resnet50_swsl"] = {
+    "model": lambda *args: torch.hub.load(
+        "facebookresearch/semi-supervised-ImageNet1K-models", "resnet50_swsl", *args
+    ).eval(),
+    "layers": [-2],
+}
 
 
 # SELF-SUPERVISED
 
-# models_dict["barlow"] = {
-#     "model": torch.hub.load("facebookresearch/barlowtwins:main", "resnet50").eval(),
-#     "layers": [-2],
-# }
+models_dict["barlow"] = {
+    "model": lambda *args: torch.hub.load(
+        "facebookresearch/barlowtwins:main", "resnet50", *args
+    ).eval(),
+    "layers": [-2],
+}
